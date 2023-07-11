5621fc599d3ed613a4fb582c76b910099030f903
Ifthakher Rahman Mohib <ifthakherrahman426@gmail.com>
2023-07-11 15:40:59 +0600

    footer added

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Footer = () => {
+	return (
+		<div class='bg-light text-center text-lg-start'>
+			<div class='text-center bg-primary p-3 text-light'>
+				© 2023 Copyright :
+				<a class='text-light' href='#'>
+					LMS Project
+				</a>
+			</div>
+		</div>
+	);
+};
+
+export default Footer;
