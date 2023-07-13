0301068cb432d4f5c49b9d1aeec4c9e9ddee1695
Jay R SG <jaysen110@gmail.com>
2023-07-13 20:35:00 +0600

    logout route set

--- src/components/admin/AdminNav.js
+++ src/components/admin/AdminNav.js
@@ -5,70 +5,70 @@ import { faAddressBook, faBook, faDashboard, faRotateLeft, faUser, faUserPlus } 
 import Link from "next/link";
 
 const AdminNav = () => {
-	const router = useRouter();
+  const router = useRouter();
 
-	return (
-		<>
-			<div className='sidebar' id='sidebar'>
-				<div className='sidebar-inner slimscroll'>
-					<div id='sidebar-menu' className='sidebar-menu'>
-						<ul>
-							<li className='menu-title'>Main</li>
+  return (
+    <>
+      <div className='sidebar' id='sidebar'>
+        <div className='sidebar-inner slimscroll'>
+          <div id='sidebar-menu' className='sidebar-menu'>
+            <ul>
+              <li className='menu-title'>Main</li>
 
-							<li className={router.pathname === "/admin" ? "active" : ""}>
-								<Link href='/admin'>
-									<FontAwesomeIcon icon={faDashboard} />
-									<span>Dashboard</span>
-								</Link>
-							</li>
+              <li className={router.pathname === "/admin" ? "active" : ""}>
+                <Link href='/admin'>
+                  <FontAwesomeIcon icon={faDashboard} />
+                  <span>Dashboard</span>
+                </Link>
+              </li>
 
-							{/* <li className={router.pathname === "/admin/adminProfile" ? "active" : ""}>
+              {/* <li className={router.pathname === "/admin/adminProfile" ? "active" : ""}>
 								<Link href='/admin/adminProfile'>
 									<FontAwesomeIcon icon={faUserPlus} />
 									<span>My Account</span>
 								</Link>
 							</li> */}
 
-							<li className={router.pathname === "/admin/bookStock" || router.pathname === "/admin/addBook" ? "active" : ""}>
-								<Link href='/admin/bookStock'>
-									<FontAwesomeIcon icon={faBook} />
-									<span>Add Book Stock</span>
-								</Link>
-							</li>
+              <li className={router.pathname === "/admin/bookStock" || router.pathname === "/admin/addBook" ? "active" : ""}>
+                <Link href='/admin/bookStock'>
+                  <FontAwesomeIcon icon={faBook} />
+                  <span>Add Book Stock</span>
+                </Link>
+              </li>
 
-							{/* <li className={router.pathname === "/user/userBookReports" ? "active" : ""}>
+              {/* <li className={router.pathname === "/user/userBookReports" ? "active" : ""}>
 								<Link href='/user/userBookReports'>
 									<FontAwesomeIcon icon={faUserPlus} />
 									<span>Add User</span>
 								</Link>
 							</li> */}
 
-							<li className={router.pathname === "/admin/issueBook" || router.pathname === "/admin/addIssueBooks" ? "active" : ""}>
-								<Link href='/admin/issueBook'>
-									<FontAwesomeIcon icon={faAddressBook} />
-									<span>Issue Books</span>
-								</Link>
-							</li>
+              <li className={router.pathname === "/admin/issueBook" || router.pathname === "/admin/addIssueBooks" ? "active" : ""}>
+                <Link href='/admin/issueBook'>
+                  <FontAwesomeIcon icon={faAddressBook} />
+                  <span>Issue Books</span>
+                </Link>
+              </li>
 
-							<li className={router.pathname === "/admin/returnBook" || router.pathname === "/admin/addReturnBooks" ? "active" : ""}>
-								<Link href='/admin/returnBook'>
-									<FontAwesomeIcon icon={faRotateLeft} />
-									<span>Return Books</span>
-								</Link>
-							</li>
+              <li className={router.pathname === "/admin/returnBook" || router.pathname === "/admin/addReturnBooks" ? "active" : ""}>
+                <Link href='/admin/returnBook'>
+                  <FontAwesomeIcon icon={faRotateLeft} />
+                  <span>Return Books</span>
+                </Link>
+              </li>
 
-							<li>
-								<Link href='/'>
-									<FontAwesomeIcon icon={faUser} />
-									<span>Logout</span>
-								</Link>
-							</li>
-						</ul>
-					</div>
-				</div>
-			</div>
-		</>
-	);
+              <li>
+                <Link href='/logout?type=admin'>
+                  <FontAwesomeIcon icon={faUser} />
+                  <span>Logout</span>
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default AdminNav;
--- src/components/user/UserNav.js
+++ src/components/user/UserNav.js
@@ -5,56 +5,56 @@ import { faBook, faDashboard, faUser, faUserPlus } from "@fortawesome/free-solid
 import Link from "next/link";
 
 const UserNav = () => {
-	const router = useRouter();
+  const router = useRouter();
 
-	return (
-		<>
-			<div className='sidebar' id='sidebar'>
-				<div className='sidebar-inner slimscroll'>
-					<div id='sidebar-menu' className='sidebar-menu'>
-						<ul>
-							<li className='menu-title'>Main</li>
+  return (
+    <>
+      <div className='sidebar' id='sidebar'>
+        <div className='sidebar-inner slimscroll'>
+          <div id='sidebar-menu' className='sidebar-menu'>
+            <ul>
+              <li className='menu-title'>Main</li>
 
-							<li className={router.pathname === "/user" ? "active" : ""}>
-								<Link href='/user'>
-									<FontAwesomeIcon icon={faDashboard} />
-									<span>Dashboard</span>
-								</Link>
-							</li>
+              <li className={router.pathname === "/user" ? "active" : ""}>
+                <Link href='/user'>
+                  <FontAwesomeIcon icon={faDashboard} />
+                  <span>Dashboard</span>
+                </Link>
+              </li>
 
-							<li className={router.pathname === "/user/userProfile" || router.pathname === "/user/userEditProfile" ? "active" : ""}>
-								<Link href='/user/userProfile'>
-									<FontAwesomeIcon icon={faUserPlus} />
-									<span>My Account</span>
-								</Link>
-							</li>
+              <li className={router.pathname === "/user/userProfile" || router.pathname === "/user/userEditProfile" ? "active" : ""}>
+                <Link href='/user/userProfile'>
+                  <FontAwesomeIcon icon={faUserPlus} />
+                  <span>My Account</span>
+                </Link>
+              </li>
 
-							<li className={router.pathname === "/user/books" || router.pathname === "/user/addBooks" ? "active" : ""}>
-								<Link href='/user/books'>
-									<FontAwesomeIcon icon={faBook} />
-									<span>Books</span>
-								</Link>
-							</li>
+              <li className={router.pathname === "/user/books" || router.pathname === "/user/addBooks" ? "active" : ""}>
+                <Link href='/user/books'>
+                  <FontAwesomeIcon icon={faBook} />
+                  <span>Books</span>
+                </Link>
+              </li>
 
-							<li className={router.pathname === "/user/userBookReports" ? "active" : ""}>
-								<Link href='/user/userBookReports'>
-									<FontAwesomeIcon icon={faBook} />
-									<span>Book Reports</span>
-								</Link>
-							</li>
+              <li className={router.pathname === "/user/userBookReports" ? "active" : ""}>
+                <Link href='/user/userBookReports'>
+                  <FontAwesomeIcon icon={faBook} />
+                  <span>Book Reports</span>
+                </Link>
+              </li>
 
-							<li>
-								<Link href='/'>
-									<FontAwesomeIcon icon={faUser} />
-									<span>Logout</span>
-								</Link>
-							</li>
-						</ul>
-					</div>
-				</div>
-			</div>
-		</>
-	);
+              <li>
+                <Link href='/logout?type=user'>
+                  <FontAwesomeIcon icon={faUser} />
+                  <span>Logout</span>
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default UserNav;
