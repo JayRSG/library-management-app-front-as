2f7bca41e5e8579d5f0186e05cfa7714d5f8536e
Jay R SG <jaysen110@gmail.com>
2023-07-14 18:57:01 +0600

    fixing next config

--- next.config.js
+++ next.config.js
@@ -1,24 +1,25 @@
 /** @type {import('next').NextConfig} */
-export const reactStrictMode = true
-
-export async function headers() {
-  return [
-    {
-      source: "/(.*)",
-      headers: [
-        {
-          key: "Access-Control-Allow-Origin",
-          value: "http://api.libraryman.com", // Replace with your backend URL
-        },
-        {
-          key: "Access-Control-Allow-Methods",
-          value: "GET, POST, PUT, DELETE",
-        },
-        {
-          key: "Access-Control-Allow-Headers",
-          value: "Content-Type",
-        },
-      ],
-    }
-  ]
+module.exports = {
+  reactStrictMode: true,
+  headers: () => {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "http://api.libraryman.com", // Replace with your backend URL
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type",
+          },
+        ]
+      }
+    ]
+  }
 }
