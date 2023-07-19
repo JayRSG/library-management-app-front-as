81d1ecb1d573f3a2ae0a93013157c07c32a19192
Jay R SG <jaysen110@gmail.com>
2023-07-19 14:03:56 +0600

    Admin Dashboard

--- src/pages/admin/index.js
+++ src/pages/admin/index.js
@@ -1,10 +1,9 @@
 import Header from "@/components/Header"
 import AdminNav from "@/components/admin/AdminNav"
-import { useAdmin } from "@/hooks/useAdmin"
+import AdminDashboard from "@/components/admin/Dashboard"
 import React from "react"
 
 const Index = () => {
-  const { data: admin, isLoading: adminLoading, error: adminError } = useAdmin({ middleware: "auth", })
 
   return (
     <>
@@ -12,13 +11,7 @@ const Index = () => {
 
       <AdminNav />
 
-      <div className='page-wrapper'>
-        <div className='content'>
-          <div>
-            <h1 style={{ textAlign: "center" }}>Admin, Welcome to your panel</h1>
-          </div>
-        </div>
-      </div>
+      <AdminDashboard />
     </>
   )
 }
