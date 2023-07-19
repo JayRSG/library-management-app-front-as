695e38b93dfe92934ca33d1479929440cac66a2e
Jay R SG <jaysen110@gmail.com>
2023-07-19 14:02:15 +0600

    Fixing routes

--- src/components/admin/AdminNav.js
+++ src/components/admin/AdminNav.js
@@ -1,12 +1,12 @@
-import { useRouter } from "next/router"
 import React from "react"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faAddressBook, faBook, faDashboard, faRotateLeft, faUser, faUserPlus } from "@fortawesome/free-solid-svg-icons"
 import Link from "next/link"
+import { resolveActive } from "@/lib/utils"
+import { useRouter } from "next/router"
 
 const AdminNav = () => {
   const router = useRouter()
-
   return (
     <>
       <div className='sidebar' id='sidebar'>
@@ -15,47 +15,55 @@ const AdminNav = () => {
             <ul>
               <li className='menu-title'>Main</li>
 
-              <li className={router.pathname === "/admin" ? "active" : ""}>
+              <li className={resolveActive(["/admin"], router.pathname)}>
                 <Link href='/admin'>
                   <FontAwesomeIcon icon={faDashboard} />
                   <span>Dashboard</span>
                 </Link>
               </li>
 
-              {/* <li className={router.pathname === "/admin/adminProfile" ? "active" : ""}>
+              {/* <li className={resolveActive(["/admin/adminProfile"])}>
 								<Link href='/admin/adminProfile'>
 									<FontAwesomeIcon icon={faUserPlus} />
 									<span>My Account</span>
 								</Link>
 							</li> */}
 
-              <li className={router.pathname === "/admin/bookStock" || router.pathname === "/admin/addBook" ? "active" : ""}>
-                <Link href='/admin/bookStock'>
+              <li className={resolveActive(["/booksLibrary", "/issueBook", "/addBooks"], router.pathname)}>
+                <Link href='/booksLibrary'>
                   <FontAwesomeIcon icon={faBook} />
-                  <span>Add Book Stock</span>
+                  <span>Book Library</span>
                 </Link>
               </li>
 
-              {/* <li className={router.pathname === "/user/userBookReports" ? "active" : ""}>
-								<Link href='/user/userBookReports'>
-									<FontAwesomeIcon icon={faUserPlus} />
-									<span>Add User</span>
-								</Link>
-							</li> */}
-
-              <li className={router.pathname === "/admin/issueBook" || router.pathname === "/admin/addIssueBooks" ? "active" : ""}>
+              <li className={resolveActive(["/admin/issueBook", "/admin/addIssueBooks"], router.pathname)}>
                 <Link href='/admin/issueBook'>
                   <FontAwesomeIcon icon={faAddressBook} />
                   <span>Issue Books</span>
                 </Link>
               </li>
 
-              <li className={router.pathname === "/admin/returnBook" || router.pathname === "/admin/addReturnBooks" ? "active" : ""}>
+              <li className={resolveActive(["/admin/returnBook", "/admin/addReturnBooks"], router.pathname)}>
                 <Link href='/admin/returnBook'>
                   <FontAwesomeIcon icon={faRotateLeft} />
                   <span>Return Books</span>
                 </Link>
               </li>
+
+              <li className={resolveActive(["/admin/bookStock", "/admin/addBook"], router.pathname)}>
+                <Link href='/admin/bookStock'>
+                  <FontAwesomeIcon icon={faBook} />
+                  <span>Add Books</span>
+                </Link>
+              </li>
+
+              {/* <li className={resolveActive(["/user/userBookReports"], router.pathname)}>
+								<Link href='/user/userBookReports'>
+									<FontAwesomeIcon icon={faUserPlus} />
+									<span>Add User</span>
+								</Link>
+							</li> */}
+
 
               <li>
                 <Link href='/logout?type=admin'>
