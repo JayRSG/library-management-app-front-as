1a7c7628f4fc67bbac5d2be3e8fddbf8f5369482
Jay R SG <jaysen110@gmail.com>
2023-07-19 14:04:10 +0600

    refactor: bootstrap min added in _App

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,5 @@
-import "bootstrap/dist/css/bootstrap.css"
+// import "bootstrap/dist/css/bootstrap.css"
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "@/styles/globals.css"
 
 import { useEffect } from "react"
