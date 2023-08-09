b361b330d0216b26e899b4416f727e1916562b04
Jay R SG <jaysen110@gmail.com>
2023-08-10 01:12:31 +0600

    Prompt fingerprint scan in issuebook

--- src/components/IssueBook.js
+++ src/components/IssueBook.js
@@ -7,11 +7,16 @@ const IssueBook = (props) => {
   const { id } = router.query
   const { auth, auth_type } = props
   const { bookdata, bookLoading } = useBooks({ params: { id: id } })
+  const [scanData, setScanData] = useState(null)
 
-  const handleSubmit = (e) => {
+  const handleInputChange = () => {
 
   }
 
+  console.log(auth_type)
+  const handleSubmit = (e) => {
+
+  }
 
   return (
     <>
@@ -34,19 +39,14 @@ const IssueBook = (props) => {
                   <div className='col-md-6'>
                     <div className='form-group'>
                       <label>Name</label>
-                      <input className='form-control' type='text' value={auth?.first_name + " " + auth?.last_name} readOnly=''
-                        disabled />
+                      <p>{(auth_type == "admin" && "Scan Fingerprint") || (auth_type == "user" && auth?.first_name + " " + auth?.last_name)}</p>
                     </div>
                   </div>
 
                   <div className='col-md-6'>
                     <div className='form-group'>
                       <label>ID, Semester Department</label>
-                      <input
-                        className='form-control'
-                        type='text'
-                        value={auth?.student_id + "; " + auth?.semester + "th semester, " + auth?.department}
-                        disabled />
+                      <p>{(auth_type == "admin" && "Scan Fingerprint") || (auth_type == "user" && auth?.student_id + "; " + auth?.semester + "th semester, " + auth?.department)}</p>
                     </div>
                   </div>
                 </div>
@@ -55,37 +55,28 @@ const IssueBook = (props) => {
                   <div className='col-md-6'>
                     <div className='form-group'>
                       <label>Issue Date</label>
-                      <input
-                        className='form-control'
-                        type='text'
-                        disabled
-                        value={(new Date()).toLocaleString('en-US', {
-                          year: 'numeric',
-                          month: "2-digit",
-                          day: "2-digit",
-                          timeZone: "Asia/Dhaka"
-                        })} />
+                      <p>{(new Date()).toLocaleString('en-UK', {
+                        year: 'numeric',
+                        month: "2-digit",
+                        day: "2-digit",
+                        timeZone: "Asia/Dhaka"
+                      })}</p>
                     </div>
                   </div>
 
                   <div className='col-md-6'>
                     <div className='form-group'>
                       <label>Book Name</label>
-                      <input
-                        className='form-control'
-                        type='text'
-                        value={bookdata?.data?.name}
-                        disabled
-                      />
+                      <p>{bookdata?.data?.name + " "} {!scanData && "[Scan Book]"}</p>
                     </div>
                   </div>
-                  <div className="col-sm-6 col-md-3">
+                  <div className="col-sm-3 col-md-2">
                     <button name="" className="btn btn-success btn-block">
                       Scan Book
                     </button>
                   </div>
 
-                  {auth_type == "admin" && <div className="col-sm-6 col-md-3">
+                  {auth_type == "admin" && <div className="col-sm-3 col-md-2">
                     <button name="" className="btn btn-danger btn-block">
                       Scan Fingerprint
                     </button>
@@ -93,7 +84,7 @@ const IssueBook = (props) => {
                 </div>
 
                 <div className='m-t-20 text-center'>
-                  <button className='btn btn-primary submit-btn'>Add Issue Book</button>
+                  <button className='btn btn-primary submit-btn' disabled={!scanData}>Issue Book</button>
                 </div>
               </form>
             </div>
