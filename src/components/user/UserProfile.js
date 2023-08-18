c329f94dc36d4c888c4181319bbe4762a75fc7ea
Jay R SG <jaysen110@gmail.com>
2023-08-18 13:49:46 +0600

    User Information updates

--- src/components/user/UserProfile.js
+++ src/components/user/UserProfile.js
@@ -8,65 +8,73 @@ const UserProfileComponent = () => {
 
   return (
     <div className='page-wrapper'>
-        <div className='content'>
-          <div className='row'>
-            <div className='col-sm-7 col-6'>
-              <h4 className='page-title'>My Profile</h4>
-            </div>
-
-            <div className='col-sm-5 col-6 text-right m-b-30'>
-              <Link href='/user/userEditProfile' className='btn btn-primary btn-rounded'>
-                <FontAwesomeIcon icon={faPlus} /> Edit Profile
-              </Link>
-            </div>
+      <div className='content'>
+        <div className='row'>
+          <div className='col-sm-7 col-6'>
+            <h4 className='page-title'>My Profile</h4>
           </div>
 
-          <div className='card-box profile-header'>
-            <div className='row'>
-              <div className='col-md-12'>
-                <div className='profile-view'>
-                  <div className='profile-basic'>
-                    <div className='row'>
-                      <div className='col-md-5'>
-                        <div className='profile-info-left'>
-                          <h3 className='user-name m-t-0 mb-0'>{user?.data?.first_name + " " + user?.data?.last_name}</h3>
-                          <small className='text-muted'>{user?.data?.department + " " + user?.data?.semester}<sup>th</sup> semester</small>
-                          <div className='staff-id'>Student ID : {user?.data?.student_id}</div>
-                        </div>
+          <div className='col-sm-5 col-6 text-right m-b-30'>
+            <Link href='/user/userEditProfile' className='btn btn-primary btn-rounded'>
+              <FontAwesomeIcon icon={faPlus} /> Edit Profile
+            </Link>
+          </div>
+        </div>
+
+        <div className='card-box profile-header'>
+          <div className='row'>
+            <div className='col-md-12'>
+              <div className='profile-view'>
+                <div className='profile-basic'>
+                  <div className='row'>
+                    <div className='col-md-5'>
+                      <div className='profile-info-left'>
+                        <h3 className='user-name m-t-0 mb-0'>{user?.data?.first_name + " " + user?.data?.last_name}</h3>
+                        <small className='text-muted'>{user?.data?.student_info?.department}</small>
+                        {
+                          user?.data?.user_type == "Student" &&
+
+                          (<>
+                            <div className='staff-id'>Student ID : {user?.data?.student_id}</div>
+                            <div className='staff-id'>Admission Year : {user?.data?.student_info?.admission_semester +  " " + user?.data?.student_info?.admission_year}</div>
+                          </>
+                          )
+
+                        }
                       </div>
+                    </div>
 
-                      <div className='col-md-7'>
-                        <ul className='personal-info'>
-                          <li>
-                            <span className='title'>Phone:</span>
-                            <span className='text'>
-                              <a href='#'>{`${user?.data?.phone}`}</a>
-                            </span>
-                          </li>
+                    <div className='col-md-7'>
+                      <ul className='personal-info'>
+                        <li>
+                          <span className='title'>Phone:</span>
+                          <span className='text'>
+                            <a href='#'>{`${user?.data?.phone}`}</a>
+                          </span>
+                        </li>
 
-                          <li>
-                            <span className='title'>Email:</span>
-                            <span className='text'>
-                              <a href='#'>{`${user?.data?.email}`}</a>
-                            </span>
-                          </li>
+                        <li>
+                          <span className='title'>Email:</span>
+                          <span className='text'>
+                            <a href='#'>{`${user?.data?.email}`}</a>
+                          </span>
+                        </li>
 
-                          {/* <li>
+                        {/* <li>
                             <span className='title'>Birthday:</span>
                             <span className='text'>1st January</span>
                           </li> */}
 
-                          {/* <li>
+                        {/* <li>
                             <span className='title'>Address:</span>
                             <span className='text'>Lalkhan Bazar, Bangladesh</span>
                           </li> */}
 
-                          {/* <li>
+                        {/* <li>
                             <span className='title'>Gender:</span>
                             <span className='text'>Male</span>
                           </li> */}
-                        </ul>
-                      </div>
+                      </ul>
                     </div>
                   </div>
                 </div>
@@ -75,6 +83,7 @@ const UserProfileComponent = () => {
           </div>
         </div>
       </div>
+    </div>
   )
 }
 export default UserProfileComponent
