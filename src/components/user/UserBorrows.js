05bf9d9d7bd6fd0c7566e5e48251e24a65b38e8c
Jay R SG <jaysen110@gmail.com>
2023-10-09 00:54:53 +0600

    Adding book return and device implementation for user

--- src/components/user/UserBorrows.js
+++ src/components/user/UserBorrows.js
@@ -1,8 +1,10 @@
 import { useBorrows } from "@/hooks/useBorrows"
 import { post } from "@/lib/axios"
-import { faEllipsisVertical, faHome, faPencil, faRecycle, faRefresh, faTentArrowTurnLeft, faTrash, faTurnUp } from "@fortawesome/free-solid-svg-icons"
+import { faEllipsisVertical, faRecycle, faRefresh } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { useEffect, useState } from "react"
+import PopupModal from "../Modal"
+import ReturnBook from "../ReturnBook"
 
 const UserBorrows = (props) => {
   const { auth } = props
@@ -14,8 +16,31 @@ const UserBorrows = (props) => {
     date_to: "",
   })
 
+  const [showModal, setShowModal] = useState(false)
+  const [modalTitle, setModalTitle] = useState("Modal Title")
+  const [modalData, setModalData] = useState(false)
+  const [saveButtonState, setSaveButtonState] = useState(false)
+
   const { borrowData, borrowLoading, borrowMutate } = useBorrows({ params: queryParams })
   const [fine, setFine] = useState([])
+  const [bookReturnData, setBookReturnData] = useState({
+    borrow_data: "",
+  })
+
+
+  const handleShowModal = () => {
+    setShowModal(true)
+    setSaveButtonState(false)
+  }
+  const handleCloseModal = () => {
+    setShowModal(false)
+    setSaveButtonState(false)
+  }
+
+  const handleOpenModal = () => {
+    setModalTitle("Return Book")
+    handleShowModal();
+  };
 
   useEffect(() => {
     if (auth) {
@@ -29,6 +54,22 @@ const UserBorrows = (props) => {
       [e.currentTarget.name]: e.currentTarget.type == "checkbox" ? (e.currentTarget.checked ? 1 : 0) : e.currentTarget.value
     })
   }
+
+  useEffect(() => {
+    if (bookReturnData?.borrow_data) {
+      setModalData(
+        <ReturnBook
+          bookBorrowData={bookReturnData?.borrow_data}
+          saveButtonState={saveButtonState}
+          handleCloseModal={handleCloseModal}
+          borrowMutate={borrowMutate}
+        />
+      )
+
+      handleOpenModal()
+    }
+  }, [bookReturnData, saveButtonState])
+
 
   const calcualteFine = async (ind, borrow_id) => {
     await post({
@@ -54,16 +95,19 @@ const UserBorrows = (props) => {
     })
   }
 
-  return (
-    <div className='page-wrapper'>
-      <div className='content'>
-        <div className='row'>
-          <div className='col-sm-12'>
-            <h4 className='page-title'>Book Issue History</h4>
-          </div>
-        </div>
 
-        {/* <div className='row filter-row'>
+
+  return (
+    <>
+      <div className='page-wrapper'>
+        <div className='content'>
+          <div className='row'>
+            <div className='col-sm-12'>
+              <h4 className='page-title'>Book Issue History</h4>
+            </div>
+          </div>
+
+          {/* <div className='row filter-row'>
           <div className='col-sm-6 col-md-3'>
             <div className='form-group'>
               <input type='text' autoFocus='' className='form-control' placeholder='search book name' />
@@ -77,109 +121,124 @@ const UserBorrows = (props) => {
           </div>
         </div> */}
 
-        <div className="row filter-row flex-d justify-content-center">
-          <div className="col-sm-2 col-md-2">
-            <label htmlFor="returned">
-              <input
-                name="returned"
-                id="returned"
-                type="checkbox"
-                style={{ marginRight: "5px" }}
-                onChange={(e) => handleFilterChange(e)} />
-              Returned
-            </label>
+          <div className="row filter-row flex-d justify-content-center">
+            <div className="col-sm-2 col-md-2">
+              <label htmlFor="returned">
+                <input
+                  name="returned"
+                  id="returned"
+                  type="checkbox"
+                  style={{ marginRight: "5px" }}
+                  onChange={(e) => handleFilterChange(e)} />
+                Returned
+              </label>
+            </div>
+            <div className="col-sm-3 col-md-6">
+              <label style={{ marginRight: "20px" }} htmlFor="returned">Date From
+                <input
+                  style={{ marginLeft: "10px" }}
+                  name="date_from"
+                  id="date_from"
+                  type="date"
+                  onChange={handleFilterChange} />
+              </label>
+              <label htmlFor="returned">Date To
+                <input
+                  style={{ marginLeft: "10px" }}
+                  name="date_to"
+                  id="date_to"
+                  type="date"
+                  onChange={handleFilterChange} />
+              </label>
+            </div>
           </div>
-          <div className="col-sm-3 col-md-6">
-            <label style={{ marginRight: "20px" }} htmlFor="returned">Date From
-              <input
-                style={{ marginLeft: "10px" }}
-                name="date_from"
-                id="date_from"
-                type="date"
-                onChange={handleFilterChange} />
-            </label>
-            <label htmlFor="returned">Date To
-              <input
-                style={{ marginLeft: "10px" }}
-                name="date_to"
-                id="date_to"
-                type="date"
-                onChange={handleFilterChange} />
-            </label>
-          </div>
-        </div>
 
-        <div className='row mt-4'>
-          <div className='col-md-12'>
-            <div className='table-responsive'>
-              {borrowData?.data?.length > 0 ?
-                <table className='table table-striped custom-table mb-0 datatable'>
-                  <thead>
-                    <tr>
-                      <th>#</th>
-                      <th>Book-Name</th>
-                      <th>Borrow Date</th>
-                      <th>Due Date</th>
-                      <th>return Date</th>
-                      <th>Fine Payment Date</th>
-                      <th>Late Fine</th>
-                      <th>Status</th>
-                      <th>Action</th>
-                    </tr>
-                  </thead>
+          <div className='row mt-4'>
+            <div className='col-md-12'>
+              <div className='table-responsive'>
+                {borrowData?.data?.length > 0 ?
+                  <table className='table table-striped custom-table mb-0 datatable'>
+                    <thead>
+                      <tr>
+                        <th>#</th>
+                        <th>Book-Name</th>
+                        <th>Borrow Date</th>
+                        <th>Due Date</th>
+                        <th>return Date</th>
+                        <th>Fine Payment Date</th>
+                        <th>Late Fine</th>
+                        <th>Status</th>
+                        <th>Action</th>
+                      </tr>
+                    </thead>
 
-                  <tbody>
-                    {
-                      borrowData?.data?.map((data, ind) => (
-                        <tr key={"borrow_data" + ind}>
-                          <td>{ind + 1}</td>
-                          <td>{data?.name}</td>
-                          <td>{data?.borrow_time}</td>
-                          <td>{data?.due_time ?? 'N/A'}</td>
-                          <td>{data?.return_time ?? "N/A"}</td>
-                          <td>{data?.fine_payment_date ?? 'N/A'}</td>
-                          <td title={"Late fine"}>
-                            {
-                              data?.late_fine ? (data?.late_fine) :
-                                fine[ind] ??
-                                (<button className="btn btn-primary" onClick={(e) => {
-                                  e.stopPropagation()
-                                  calcualteFine(ind, data?.id)
-                                }}>Calculate Fine</button>)
-                            }
-                          </td>
-                          <td>{data?.returned == "1" ? "Returned" : data?.returned == "0" && "Pending"}</td>
-                          <td>
-                            <div className='dropdown dropdown-action' style={{ position: "absolute" }}>
-                              <a href='#' className='action-icon dropdown-toggle' data-bs-toggle='dropdown' aria-expanded='false'>
-                                <FontAwesomeIcon icon={faEllipsisVertical} style={{ bottom: 10, position: "relative" }} />
-                              </a>
-                              <div className='dropdown-menu dropdown-menu-right'>
-                                <span className='dropdown-item' style={{ cursor: "pointer" }}>
-                                  <FontAwesomeIcon icon={faHome} style={{ marginRight: 10 }} /> Return
-                                </span>
-                                <span className='dropdown-item' style={{ cursor: "pointer" }} onClick={(e) => {
-                                  e.stopPropagation()
-                                  borrowMutate()
-                                  calcualteFine(ind, data?.id)
-                                }}>
-                                  <FontAwesomeIcon icon={faRefresh} style={{ marginRight: 10 }} /> Refresh
-                                </span>
+                    <tbody>
+                      {
+                        borrowData?.data?.map((data, ind) => (
+                          <tr key={"borrow_data" + ind}>
+                            <td>{ind + 1}</td>
+                            <td>{data?.name}</td>
+                            <td>{data?.borrow_time}</td>
+                            <td>{data?.due_time ?? 'N/A'}</td>
+                            <td>{data?.return_time ?? "N/A"}</td>
+                            <td>{data?.fine_payment_date ?? 'N/A'}</td>
+                            <td title={"Late fine"}>
+                              {
+                                data?.late_fine ? (data?.late_fine) :
+                                  fine[ind] ??
+                                  (<button className="btn btn-primary" onClick={(e) => {
+                                    e.stopPropagation()
+                                    calcualteFine(ind, data?.id)
+                                  }}>Calculate Fine</button>)
+                              }
+                            </td>
+                            <td>{data?.returned == "1" ? "Returned" : data?.returned == "0" && "Pending"}</td>
+                            <td>
+                              <div className='dropdown dropdown-action' style={{ position: "absolute" }}>
+                                <a href='#' className='action-icon dropdown-toggle' data-bs-toggle='dropdown' aria-expanded='false'>
+                                  <FontAwesomeIcon icon={faEllipsisVertical} style={{ bottom: 10, position: "relative" }} />
+                                </a>
+                                <div className='dropdown-menu dropdown-menu-right'>
+                                  <span className='dropdown-item' style={{ cursor: "pointer" }}
+                                    onClick={(e) => {
+                                      e.stopPropagation()
+                                      setBookReturnData({ borrow_data: data })
+                                    }}>
+                                    <FontAwesomeIcon icon={faRecycle} style={{ marginRight: 10 }} /> Return
+                                  </span>
+
+                                  <span className='dropdown-item' style={{ cursor: "pointer" }}
+                                    onClick={(e) => {
+                                      e.stopPropagation()
+                                      borrowMutate()
+                                      calcualteFine(ind, data?.id)
+                                    }}>
+                                    <FontAwesomeIcon icon={faRefresh} style={{ marginRight: 10 }} /> Refresh
+                                  </span>
+                                </div>
                               </div>
-                            </div>
-                          </td>
-                        </tr>
-                      ))
-                    }
-                  </tbody>
-                </table>
-                : <div className="d-flex justify-content-center"><div className="text-center">No Data Found</div></div>
-              }
+                            </td>
+                          </tr>
+                        ))
+                      }
+                    </tbody>
+                  </table>
+                  : <div className="d-flex justify-content-center"><div className="text-center">No Data Found</div></div>
+                }
+              </div>
             </div>
           </div>
         </div>
+
       </div>
-    </div>
+      <PopupModal
+        title={modalTitle}
+        showModal={showModal}
+        handleCloseModal={handleCloseModal}
+        data={modalData}
+        saveButtonAction={setSaveButtonState}
+      />
+    </>
   )
 }
 
