00534f3a258ea2e04c120deb4a564b40fc79a351
Jay R SG <jaysen110@gmail.com>
2023-10-09 00:36:51 +0600

    Fingerprint management module

--- src/components/admin/EditFingerprint.js
+++ src/components/admin/EditFingerprint.js
@@ -8,6 +8,7 @@ const EditFingerPrint = (props) => {
   const { userData, userType, submitButtonAction, saveButtonState, handleCloseModal } = props
 
   const { deviceData, isScanning, socket, communicateDevice } = useDeviceData()
+  const [fingerprintId, setFingerprintId] = useState(null)
   const [showMessage, setShowMessage] = useState("")
   const [verified, setVerified] = useState(false)
   const [reassignState, setReassignedState] = useState({
@@ -35,12 +36,12 @@ const EditFingerPrint = (props) => {
       })
   }
 
-  async function storeFingerprint(fingerprint) {
+  async function storeFingerprint(fingerprintId) {
     return await fetcher({
       url: "/user/fingerprint_manage",
       params: {
         operation: "store",
-        fingerprint: fingerprint
+        fingerprint_id: fingerprintId
       }
     })
       .then(res => res?.data)
@@ -69,10 +70,8 @@ const EditFingerPrint = (props) => {
 
       setShowMessage(data)
 
-      if (data?.message == "Enrollment Successful" && saveButtonState && !reassignState?.fingerId && !reassignState?.userId) {
-        const fingerprint = data?.id
-        
-        storeFingerprint(fingerprint)
+      if (data?.message == "Enrollment Successful" && fingerprintId && saveButtonState && !reassignState?.fingerId && !reassignState?.userId) {
+        storeFingerprint(fingerprintId)
           .then(res => {
             const FingerprintSlot = res
             console.log(FingerprintSlot)
@@ -93,7 +92,7 @@ const EditFingerPrint = (props) => {
         }
       }
 
-      if (data?.message == "Fingerprint matched") {
+      if (data?.message == "Fingerprint Matched") {
         setVerified(true)
       }
     }
@@ -101,16 +100,18 @@ const EditFingerPrint = (props) => {
 
   async function performFingerprintActions() {
     if (!isScanning) {
-      if (userData?.fingerprint != null || userData?.fingerprint == 0) {
+      if (userData?.fingerprint_id != null) {
+        console.log("Fingerprint found, verifying now!");
         setShowMessage("Verify")
         setVerified(false)
 
-        communicateDevice({ command: "verify", id: userData?.fingerprint });
+        communicateDevice({ command: "verify", id: userData?.fingerprint_id });
       } else {
-        const fingerprintId = await getFingerprintId()
+        const retrievedFingerId = await getFingerprintId()
+        setFingerprintId(retrievedFingerId)
         setShowMessage("Enroll New Fingerprint")
 
-        communicateDevice({ command: "enroll", id: fingerprintId });
+        communicateDevice({ command: "enroll", id: retrievedFingerId });
       }
     }
   }
@@ -127,7 +128,7 @@ const EditFingerPrint = (props) => {
       <div className='text-center'>
         <h4 className='text-xl' onClick={(e) => {
           e.stopPropagation()
-          performFingerprintActions
+          performFingerprintActions()
         }}>{showMessage?.action || "Press to start"}</h4>
 
         <div>{showMessage?.message}</div>
@@ -140,7 +141,7 @@ const EditFingerPrint = (props) => {
               <button className='bg-info text-white rounded form-control' style={{ marginTop: "50px" }}
                 onClick={(e) => {
                   e.stopPropagation()
-                  reAssignFinger(userData?.id, userData?.fingerprint)
+                  reAssignFinger(userData?.id, userData?.fingerprint_id)
                 }}>Re-assign Fingerprint</button>
               <button className='bg-danger text-white rounded form-control' style={{ marginTop: "10px" }}>Delete Fingerprint</button>
             </div> : ""
